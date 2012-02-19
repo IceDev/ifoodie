1bd33d4d936f5fff291238cea7dc89706d90757a
Colin Thomas <ti.colinthomas@gmail.com>
2012-02-19 20:10:43 +0530

    Initial configuration with templating.
    Changed default routes.
    Autoloading URL helper.
    Added templating.
    Added Bootstrap.
    Added jQuery.

---
+++ CI/application/views/template/header.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<link href="<?php echo base_url(); ?>resources/bootstrap/css/bootstrap.css" rel="stylesheet">
+		<script type="text/javascript" src="<?php echo base_url(); ?>resources/jquery/jquery.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row" style="padding-top: 10px;">
+			<!-- Header content goes here -->
+				<div class="span10 offset1">
+					<ul class="thumbnails">
+						<li class="span10">
+							<a href="#" class="thumbnail">
+								<img src="<?php echo base_url();?>resources/images/ifoodie_logo.png" style="max-width: 70%">
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
---
+++ CI/application/views/template/template.php
@@ -0,0 +1,5 @@
+<?php
+	$this->load->view('template\header');
+	$this->load->view('start_screen');
+	$this->load->view('template\footer');
+?>
